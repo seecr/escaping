897befd48ec5d65f6e4aab81376d9415ef4eca15
Johan Jonkers <johan@seecr.nl>
2015-01-06 15:37:54 +0100

    JJ: fixed detection

--- test/alltests.sh
+++ test/alltests.sh
@@ -26,17 +26,10 @@
 
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
-pyversions="python2.6"
-if [ -e /usr/bin/python2.7 ]; then
-    pyversions="$pyversions python2.7"
-fi
-if [ -e /usr/bin/python3.2 ]; then
-    pyversions="$pyversions python3.2"
-fi
-if [ "${option:0:10}" == "--python2." ]; then
-    shift
-    pyversions="${option:2}"
-fi
+pyversions=""
+test -e /usr/bin/python3.2 && pyversions="${pyversions} python3.2"
+test -e /usr/bin/python3.4 && pyversions="${pyversions} python3.4"
+
 echo Found Python versions: $pyversions
 for pycmd in $pyversions; do
     echo "================ $pycmd _alltests.py $@ ================"
