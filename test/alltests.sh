447a3a53d918d38197d13581be9952899121dd26
Johan Jonkers <johan@seecr.nl>
2019-09-26 06:04:03 -0400

    JJ: python2

--- test/alltests.sh
+++ test/alltests.sh
@@ -27,15 +27,4 @@
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
 
-pyversions=""
-test -e /usr/bin/python2.6 && pyversions="$pyversions python2.6"
-test -e /usr/bin/python2.7 && pyversions="$pyversions python2.7"
-if [ "${option:0:10}" == "--python2." ]; then
-    shift
-    pyversions="${option:2}"
-fi
-echo Found Python versions: $pyversions
-for pycmd in $pyversions; do
-    echo "================ $pycmd _alltests.py $@ ================"
-    $pycmd _alltests.py "$@"
-done
+python2 _alltests.py "$@"
